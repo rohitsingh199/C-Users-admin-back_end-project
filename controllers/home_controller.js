0ea2862d1ffcba09ced4f9ba3b2b3a915552d0c3
adisharma7 <adisharma19998@gmail.com>
2020-06-24 18:24:26 +0530

    view engine added

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,6 @@
 module.exports.home=function (req,res){
-    return res.end("<h1>express is up for coding</h1>")
+   // return res.end("<h1>express is up for coding</h1>")
+   return res.render("home",{
+       title:"home"
+   });
 }
