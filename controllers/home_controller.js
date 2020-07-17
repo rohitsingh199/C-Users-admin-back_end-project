9646ef41c089b1c80068f6b7594f71aebbbc87b8
adisharma7 <adisharma19998@gmail.com>
2020-07-17 21:55:34 +0530

    passport.js added sass added post feture added + showing the name who posted it

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,28 @@
+const Post=require("../models/post");
+const { findById } = require("../models/post");
+
 module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+    
+    // by this method below only the user id will be shown withg the post 
+    // Post.find({},function(err,posts){
+    //     return res.render('home', {
+    //         title: "Codiel Home",
+    //         posts:posts
+    //     });
+
+
+    // });
+// bpopulating the user of each post with the id and other info of the userid
+    Post.find({}).populate("user").exec(function(err,posts){
+        return res.render('home', {
+            title: "Codiel Home",
+            posts:posts
+        });
     });
+
+    
 }
 
 // module.exports.actionName = function(req, res){}
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -10,6 +10,9 @@ module.exports.profile = function(req, res){
 
 // render the sign up page
 module.exports.signUp = function(req, res){
+    if(req.isAuthenticated()){
+       return res.redirect("/users/profile");
+    }
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     })
@@ -18,6 +21,10 @@ module.exports.signUp = function(req, res){
 
 // render the sign in page
 module.exports.signIn = function(req, res){
+
+    if(req.isAuthenticated()){
+      return   res.redirect("/users/profile");
+    }
     return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     })
@@ -26,7 +33,7 @@ module.exports.signIn = function(req, res){
 // get the sign up data
 module.exports.create = function(req, res){
     if (req.body.password != req.body.confirm_password){
-        return res.redirect("back");
+        return res.redirect('back');
     }
 
     User.findOne({email: req.body.email}, function(err, user){
@@ -48,5 +55,10 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    // TODO later
+    return res.redirect('/');
+}
+
+module.exports.destroySession=function(req,res){
+    req.logout();
+    return res.redirect("/");
 }
